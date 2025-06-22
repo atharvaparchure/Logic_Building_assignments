1c8679b021ebdc835114e58ec46855bcc3b740bd
Atharva Parchure <atharvaparchure01@gmail.com>
2025-06-22 19:52:02 +0530

    Add files via upload

---
+++ Assignment_18/1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Difference(int arr[], int iSize)
+{
+    int Esum = 0, Osum = 0 , iDiff = 0, iCnt = 0;
+    for(iCnt = 0 ; iCnt < iSize; iCnt++)
+    {
+        if((arr[iCnt] % 2) == 0)
+        {
+            Esum +=arr[iCnt];
+        }
+        else if((arr[iCnt] % 2) == 1)
+        {
+            Osum += arr[iCnt];
+        }
+    }
+    iDiff = Esum - Osum;
+    return iDiff;
+
+
+}
+
+int main()
+{
+    int iValue = 0, iCnt = 0, iRet = 0 ;
+    int *p = NULL;
+    printf("Enter number of elements:\n");
+    scanf("%d",&iValue);
+    p = (int *)malloc(iValue * sizeof(int));
+    if(p == NULL)
+    {
+        printf("Unablle to allocate memory");
+        return -1;
+    }
+
+
+    printf("Enter %d elements:\n", iValue);
+    for(iCnt = 0; iCnt < iValue; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+    iRet = Difference(p , iValue);
+    printf("The difference between the summation of the even and odd numbers is %d", iRet);
+    free(p);
+    return 0;
+
+}
---
+++ Assignment_18/5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Difference(int arr[], int iSize)
+{
+    int iCnt = 0;
+    printf("Elements exactly divisible by  are :");
+    for(iCnt = 0 ; iCnt < iSize; iCnt++)
+    {
+     
+        if((arr[iCnt] % 11) == 0)
+        {
+            printf("%d\t", arr[iCnt]);
+        }
+       
+    }
+    
+    printf("\n");
+
+
+}
+
+int main()
+{
+    int iValue = 0, iCnt = 0;
+    int *p = NULL;
+    printf("Enter number of elements:\n");
+    scanf("%d",&iValue);
+    p = (int *)malloc(iValue * sizeof(int));
+    if(p == NULL)
+    {
+        printf("Unablle to allocate memory");
+        return -1;
+    }
+
+
+    printf("Enter %d elements:\n", iValue);
+    for(iCnt = 0; iCnt < iValue; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+    Difference(p , iValue);
+    
+    free(p);
+    return 0;
+
+}
